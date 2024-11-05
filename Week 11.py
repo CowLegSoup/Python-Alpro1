4c425536bfe6ace1a53395c1c3983b2c980c5383
whosblub <robertusgeraldyn34@gmail.com>
2024-11-05 15:28:23 +0700

    Update Week 11.py

--- Week 11.py
+++ Week 11.py
@@ -1,7 +1,7 @@
 def reverse(s):
     if len(s)<2:
-        return (s) 
+        return s
     first, rest = s[0],s[1:]
     return reverse(rest)+first
     
-reverse('kasur')
+reverse('erlang suka ciwa')
